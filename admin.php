0be908986647154e7434bfbd4c844045ab6b7d1c
Aadit Sharma <110082421+Amaterasu720@users.noreply.github.com>
2023-07-11 17:48:23 +0530

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ADMIN POWER</h1>
+<a href="query1.php">insert and delete values in tables using query</a></p>
+<a href="query2.php">see tables using query</a></p>
+<a href="student_table.php">Student Table</a></p>
+<a href="curr_student_table.php">Curr_Student Table</a></p>
+<a href="com_post_table.php">Company Post Table</a></p>
+</body>
+</html>
